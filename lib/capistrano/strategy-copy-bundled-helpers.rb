c2329521169215f78ec63eccdd7f28cd892b4247
Jacob Sarvis <jsarvis@users.noreply.github.com>
2015-09-25 17:05:05 -0400

    Fix typo in qoutes

--- lib/capistrano/strategy-copy-bundled-helpers.rb
+++ lib/capistrano/strategy-copy-bundled-helpers.rb
@@ -2,8 +2,8 @@ Capistrano::Configuration.instance(true).load do
   namespace :bundle do
     desc 'Creates a back up of local gems to use during rollback'
     task :backup_gems do
-      on_rollback { run "cd #{File.join(shared_path, "bundle")} && tar xf /tmp/#{application}-Gems.tar && rm /tmp/#{application}-Gems.tar'" }
-      run "cd #{File.join(shared_path, "bundle")} && tar cf /tmp/#{application}-Gems.tar ."
+      on_rollback { run "cd #{File.join(shared_path, 'bundle')} && tar xf /tmp/#{application}-Gems.tar && rm /tmp/#{application}-Gems.tar" }
+      run "cd #{File.join(shared_path, 'bundle')} && tar cf /tmp/#{application}-Gems.tar ."
     end
 
     task :remove_gem_backup do
